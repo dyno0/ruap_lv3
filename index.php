ab43667a97a912efdc8c1234e1225b6cfbb3fc24
magdalena <magdalena.tomasic1996@gmail.com>
2019-11-20 17:08:05 +0100

    changed

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ Email <input type="text" name="email" id="email"/></br>
 </form>
 <?php
 // DB connection info
-//TODO: Update the values for $host, $user, $pwd, and $db
+//TODO: Update the values for $host, $user, $pwd, and $db  
 //using the values you retrieved earlier from the Azure Portal.
 $host = "dbrkic1-mysqldbserver.mysql.database.azure.com";
 $user = "dbrkic@dbrkic1-mysqldbserver";
